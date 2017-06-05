312765e8696fa56a43f7620ab4f0f8cb2b2038bc
Vanei Anderson Heidemann <vanei.heidemann@gmail.com>
2017-06-05 17:19:29 +0000

    Iniciado implementacao do Logiqx

---
+++ logiqx/common/src/main/java/com/javanei/retrocenter/logiqx/Datafile.java
@@ -0,0 +1,11 @@
+package com.javanei.retrocenter.logiqx;
+
+import java.io.Serializable;
+
+/**
+ * 
+ */
+public class Datafile implements Serializable {
+    private String build;
+    private Boolean debug = Boolean.FALSE;
+}
---
+++ logiqx/common/src/main/java/com/javanei/retrocenter/logiqx/Header.java
@@ -0,0 +1,7 @@
+package com.javanei.retrocenter.logiqx;
+
+import java.io.Serializable;
+
+public class Header implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
