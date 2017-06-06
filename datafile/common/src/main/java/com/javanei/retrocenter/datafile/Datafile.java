a6b890f70648a649c0c7313b0564dafa49f1d0b5
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2017-06-06 16:54:10 -0300

    Criado projeto Datafile

---
+++ datafile/common/src/main/java/com/javanei/retrocenter/datafile/Datafile.java
@@ -0,0 +1,7 @@
+package com.javanei.retrocenter.datafile;
+
+import java.io.Serializable;
+
+public class Datafile implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
---
+++ datafile/common/src/main/java/com/javanei/retrocenter/datafile/Header.java
@@ -0,0 +1,54 @@
+package com.javanei.retrocenter.datafile;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Header implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * clrmamepro.name, logiqx.name
+     */
+    private String name;
+    /**
+     * clrmamepro.description, logiqx.description
+     */
+    private String description;
+    /**
+     * clrmamepro.category, logiqx.category
+     */
+    private String category;
+    /**
+     * clrmamepro.version, logiqx.version
+     */
+    private String version;
+    /**
+     * clrmamepro.author, logiqx.author
+     */
+    private String author;
+    /**
+     * logiqx.date
+     */
+    private String date;
+    /**
+     * logiqx.email
+     */
+    private String email;
+    /**
+     * logiqx.homepage
+     */
+    private String homepage;
+    /**
+     * logiqx.url
+     */
+    private String url;
+    /**
+     * logiqx.comment
+     */
+    private String comment;
+    /**
+     * Adicional not standard fields
+     */
+    private Map<String, String> customFields = new HashMap<>();
+}
