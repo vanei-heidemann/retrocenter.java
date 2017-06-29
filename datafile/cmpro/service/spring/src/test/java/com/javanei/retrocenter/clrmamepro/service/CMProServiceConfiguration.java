5c4cf113f7ed3813d892df922787ce273d9a8da8
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2017-06-29 17:54:17 -0300

    Implementado testes para o CMProService

---
+++ datafile/cmpro/service/spring/src/test/java/com/javanei/retrocenter/clrmamepro/service/CMProServiceConfiguration.java
@@ -0,0 +1,84 @@
+package com.javanei.retrocenter.clrmamepro.service;
+
+import com.javanei.retrocenter.clrmamepro.persistence.CMProCustomFieldDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProDatafileDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProDiskDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProGameDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProGameRomDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProResourceDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProResourceRomDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProSampleDAO;
+import com.javanei.retrocenter.clrmamepro.persistence.CMProSampleofDAO;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+
+public class CMProServiceConfiguration {
+    @Bean
+    public CMProService cmProService() {
+        return new CMProService();
+    }
+
+    @Bean
+    public CMProCustomFieldDAO customFieldDAO() {
+        CMProCustomFieldDAO dao = Mockito.mock(CMProCustomFieldDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProDatafileDAO datafileDAO() {
+        CMProDatafileDAO dao = Mockito.mock(CMProDatafileDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProDiskDAO diskDAO() {
+        CMProDiskDAO dao = Mockito.mock(CMProDiskDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProGameDAO gameDAO() {
+        CMProGameDAO dao = Mockito.mock(CMProGameDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProGameRomDAO gameRomDAO() {
+        CMProGameRomDAO dao = Mockito.mock(CMProGameRomDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProResourceDAO resourceDAO() {
+        CMProResourceDAO dao = Mockito.mock(CMProResourceDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProResourceRomDAO resourceRomDAO() {
+        CMProResourceRomDAO dao = Mockito.mock(CMProResourceRomDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProSampleDAO sampleDAO() {
+        CMProSampleDAO dao = Mockito.mock(CMProSampleDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+
+    @Bean
+    public CMProSampleofDAO sampleofDAO() {
+        CMProSampleofDAO dao = Mockito.mock(CMProSampleofDAO.class);
+        Mockito.when(dao.saveAndFlush(Mockito.any())).then(AdditionalAnswers.returnsFirstArg());
+        return dao;
+    }
+}
---
+++ datafile/cmpro/service/spring/src/test/java/com/javanei/retrocenter/clrmamepro/service/CMProServiceTest.java
@@ -0,0 +1,144 @@
+package com.javanei.retrocenter.clrmamepro.service;
+
+import com.javanei.retrocenter.clrmamepro.CMProDatafile;
+import com.javanei.retrocenter.clrmamepro.CMProDisk;
+import com.javanei.retrocenter.clrmamepro.CMProGame;
+import com.javanei.retrocenter.clrmamepro.CMProHeader;
+import com.javanei.retrocenter.clrmamepro.CMProResource;
+import com.javanei.retrocenter.clrmamepro.CMProRom;
+import com.javanei.retrocenter.clrmamepro.parser.CMProParser;
+import com.javanei.retrocenter.common.DatafileCategoryEnum;
+import java.io.InputStream;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {CMProServiceConfiguration.class})
+public class CMProServiceTest {
+
+    private static CMProDatafile datafile;
+    private static CMProHeader header;
+    private static CMProGame game;
+    private static CMProResource resource;
+    private static CMProDisk disk;
+    private static CMProRom rom;
+
+    @Autowired
+    private CMProService cmProService;
+
+    @BeforeClass
+    public static void initialize() throws Exception {
+        CMProParser parser = new CMProParser();
+
+        InputStream is = CMProServiceTest.class.getClassLoader().getResourceAsStream("cmpro-test.dat");
+        datafile = parser.parse(is);
+        header = datafile.getHeader();
+        game = datafile.getGames().iterator().next();
+        resource = datafile.getResources().iterator().next();
+    }
+
+    @Test
+    public void createHeader() {
+        CMProDatafile d = cmProService.create(datafile);
+        CMProHeader h = d.getHeader();
+
+        Assert.assertEquals("name", "Sony - PlayStation Vita", h.getName());
+        Assert.assertEquals("description", "Sony - PlayStation Vita", h.getDescription());
+        Assert.assertEquals("version", "20170114-224204", h.getVersion());
+        Assert.assertEquals("category", DatafileCategoryEnum.NoIntro.name(), h.getCategory());
+        Assert.assertEquals("author", "Densetsu, einstein95, Gefflon, Hiccup, jimmsu, Money_114, SonGoku, xuom2, zg", h.getAuthor());
+        Assert.assertEquals("homepage", "No-Intro", h.getHomepage());
+        Assert.assertEquals("url", "http://www.no-intro.org", h.getUrl());
+        Assert.assertNull("forcemerging", h.getForcemerging());
+        Assert.assertNull("forcezipping", h.getForcezipping());
+    }
+
+    @Test
+    public void createGame() {
+        CMProDatafile d = cmProService.create(datafile);
+        CMProGame g = d.getGames().iterator().next();
+
+        Assert.assertEquals("name", "Aegis of Earth - Protonovus Assault (USA)", g.getName());
+        Assert.assertEquals("description", "Aegis of Earth - Protonovus Assault (USA)", g.getDescription());
+        Assert.assertEquals("year", "2016", g.getYear());
+        Assert.assertEquals("manufacturer", "Manufacturer 01", g.getManufacturer());
+        Assert.assertEquals("cloneof", "cloneof 01", g.getCloneof());
+        Assert.assertEquals("romof", "romof 01", g.getRomof());
+        Assert.assertEquals("serial", "PCSE-00844", g.getSerial());
+    }
+
+    @Test
+    public void createRom() {
+        CMProDatafile d = cmProService.create(datafile);
+        CMProGame g = d.getGames().iterator().next();
+        CMProRom r = g.getRoms().iterator().next();
+
+        Assert.assertEquals("name", "Aegis of Earth - Protonovus Assault (USA).vpk", r.getName());
+        Assert.assertEquals("size", Long.valueOf(825418380), r.getSize());
+        Assert.assertEquals("crc", "BBA209A4", r.getCrc());
+        Assert.assertEquals("sha1", "9E58A114387A4DAF2033063C836EFEFDFF4EEE58", r.getSha1());
+        Assert.assertEquals("md5", "1293854EC3B83DABA1595249CA637725", r.getMd5());
+        Assert.assertEquals("region", "Brazil", r.getRegion());
+        Assert.assertEquals("flags", "verified", r.getFlags());
+    }
+
+    @Test
+    public void createDisk() {
+        CMProDatafile dat = cmProService.create(datafile);
+        CMProGame g = dat.getGames().iterator().next();
+        CMProDisk d = g.getDisks().iterator().next();
+
+        Assert.assertEquals("name", "disk 01", d.getName());
+        Assert.assertEquals("sha1", "9E58A114387A4DAF2033063C836EFEFDFF4EEE58", d.getSha1());
+        Assert.assertEquals("md5", "1293854EC3B83DABA1595249CA637725", d.getMd5());
+    }
+
+    @Test
+    public void createSamplesof() {
+        CMProDatafile dat = cmProService.create(datafile);
+        CMProGame g = dat.getGames().iterator().next();
+        String s = g.getSampleof().iterator().next();
+
+        Assert.assertEquals("sampleof", "sampleof 01", s);
+    }
+
+    @Test
+    public void createSamples() {
+        CMProDatafile dat = cmProService.create(datafile);
+        CMProGame g = dat.getGames().iterator().next();
+        String s = g.getSamples().iterator().next();
+
+        Assert.assertEquals("sample", "sample 01", s);
+    }
+
+    @Test
+    public void createResource() {
+        CMProDatafile dat = cmProService.create(datafile);
+        CMProResource r = dat.getResources().iterator().next();
+
+        Assert.assertEquals("name", "Aegis of Earth - Protonovus Assault (JP)", r.getName());
+        Assert.assertEquals("description", "Aegis of Earth - Protonovus Assault (JP)", r.getDescription());
+        Assert.assertEquals("year", "2016", r.getYear());
+        Assert.assertEquals("manufacturer", "Manufacturer 01", r.getManufacturer());
+    }
+
+    @Test
+    public void createResourceRom() {
+        CMProDatafile dat = cmProService.create(datafile);
+        CMProResource rr = dat.getResources().iterator().next();
+        CMProRom r = rr.getRoms().iterator().next();
+
+        Assert.assertEquals("name", "Aegis of Earth - Protonovus Assault (JP).vpk", r.getName());
+        Assert.assertEquals("size", Long.valueOf(825418380), r.getSize());
+        Assert.assertEquals("crc", "BBA209A4", r.getCrc());
+        Assert.assertEquals("sha1", "9E58A114387A4DAF2033063C836EFEFDFF4EEE58", r.getSha1());
+        Assert.assertEquals("md5", "1293854EC3B83DABA1595249CA637725", r.getMd5());
+        Assert.assertEquals("region", "Brazil", r.getRegion());
+        Assert.assertEquals("flags", "verified", r.getFlags());
+    }
+}
