8ba4d6214ed28f8ecadda9514a08b832afee8107
Vanei Anderson Heidemann <vanei.heidemann@fluig.com>
2017-07-08 11:07:56 -0300

    Implementado tratamento manual para umas data errada no HyperList

--- datafile/hyperlist/parser/src/main/java/com/javanei/retrocenter/hyperlist/parser/HyperListParser.java
+++ datafile/hyperlist/parser/src/main/java/com/javanei/retrocenter/hyperlist/parser/HyperListParser.java
@@ -98,6 +98,52 @@ public class HyperListParser implements Parser {
                                         ReflectionUtil.setValueByNodeContent(game, n1);
                                 }
                             }
+                            //Corrige alguns bugs no Datafile!!!
+                            if (game.getName().equals("Million Shot (Shoot-'Em-Up)(IJI Team)")
+                                    && game.getYear().equals("Shoot-'Em-Up")) {
+                                //SHARP X68000
+                                game.setYear("1993");
+                            } else if (game.getName().equals("Chuka Taisen (Sharp - SPS)")
+                                    && "Sharp - SPS".equals(game.getYear())) {
+                                //SHARP X68000
+                                game.setYear("1991");
+                            } else if (game.getYear() != null) {
+                                switch (game.getYear().trim()) {
+                                    //Nintendo Super Game Boy
+                                    case "1989 *special default palette":
+                                    case "1995 *Custom SGB Music, palette changes disabled":
+                                    case "1995 *Multiple Borders":
+                                    case "1996 *Multiple Borders":
+                                    case "1996 *Two Player Mode":
+                                    case "1998 * Multiplayer up to 4 players, Custom SGB SFX":
+                                    case "1994 *special border featuring the Chikkuns":
+                                    case "1990 *special default palette":
+                                    case "2000 *Multiple Borders":
+                                    case "1991 *special default palette":
+                                    case "1996 *Custom SGB Music":
+                                    case "1997 *Multiple Borders":
+                                    case "1998 *Multiple Borders":
+                                    case "1999 *Multiple Borders":
+                                    case "1995 *Two Player Mode":
+                                    case "1997 *Two Player Mode, Uses SGB's internal sounds":
+                                    case "1998 *Multiple Borders, Two Player Mode, Uses SGB's internal sounds":
+                                    case "1992 *special default palette":
+                                    case "1995 *Uses SGB's internal sounds":
+                                    case "1993 *special default palette":
+                                    case "1994 *Two Player Mode":
+                                    case "1999 *Multiple borders, full SNES 16-bit version":
+                                    case "1995 *Multiple Borders, SGB Two Player Mode":
+                                    case "1993 *Multiple Borders":
+                                    case "1994 *Multiplayer up to 4 players, Custom SGB SFX":
+                                    case "1994 *special default palette":
+                                    case "1994 *Multiple Borders":
+                                    case "1998 *Custom SGB Music":
+                                    case "1995 *SGB Two Player Mode":
+                                    case "1999 *Multiple borders, full SNES 16-bit version with enhanced music and graphics":
+                                        game.setYear(game.getYear().substring(0, 4));
+                                        break;
+                                }
+                            }
                             r.addGame(game);
                             break;
                         default:
@@ -105,6 +151,7 @@ public class HyperListParser implements Parser {
                     }
                 }
             }
+
         }
         return r;
     }
