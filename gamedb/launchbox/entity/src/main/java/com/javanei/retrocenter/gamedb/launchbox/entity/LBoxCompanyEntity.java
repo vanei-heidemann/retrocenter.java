c792bd993861df451a94c0c0285f612842c89481
Vanei Heidemann <vanei.heidemann@gmail.com>
2017-08-02 09:28:48 -0300

    Adicionado métodos faltantes

--- gamedb/launchbox/entity/src/main/java/com/javanei/retrocenter/gamedb/launchbox/entity/LBoxCompanyEntity.java
+++ gamedb/launchbox/entity/src/main/java/com/javanei/retrocenter/gamedb/launchbox/entity/LBoxCompanyEntity.java
@@ -28,6 +28,14 @@ public class LBoxCompanyEntity implements Serializable {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
